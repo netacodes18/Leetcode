dccb6f543e513334a61c665d98859fb7032a8670
Utkarsh Pratap <utkarsh.2023ug2037@iiitranchi.ac.in>
2026-07-12 00:20:59 +0530

    Time: 7 ms (60.02%), Space: 20.3 MB (82.07%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
@@ -1,40 +1,31 @@
 class Solution {
 public:
     int minDeletions(string s) {
-        unordered_map<char, int> mp;
+        unordered_map<char,int> mp;
 
-        // Count frequency
-        for (char c : s)
-            mp[c]++;
+        for(char c : s) mp[c]++;
 
-        vector<pair<char, int>> freq;
-        for (auto &it : mp)
-            freq.push_back(it);
+        vector<pair<char,int>> freq;
+        for(auto it : mp) freq.push_back(it);
 
         int ans = 0;
 
-        // Process every character
-        for (int i = 0; i < freq.size(); i++) {
+        for(int i = 0; i < freq.size(); i++) {
 
-            while (freq[i].second > 0) {
+            while(freq[i].second > 0) {
 
-                bool found = false;
+                int j;
+                for(j = 0; j < freq.size(); j++) {
+                    if(i == j) continue;
 
-                // Check if any other character has same frequency
-                for (int j = 0; j < freq.size(); j++) {
-
-                    if (i == j)
-                        continue;
-
-                    if (freq[i].second == freq[j].second) {
+                    if(freq[i].second == freq[j].second) {
                         freq[i].second--;
                         ans++;
-                        found = true;
                         break;
                     }
                 }
 
-                if (!found)
+                if(j == freq.size())
                     break;
             }
         }
