85147cd68b10d79ed4bac177e9501cabafb1dc9b
Utkarsh Pratap <utkarsh.2023ug2037@iiitranchi.ac.in>
2026-08-19 12:08:49 +0530

    Time: 190 ms (5.07%), Space: 92.3 MB (7.26%) - LeetHub

---
+++ 1386-cinema-seat-allocation/1386-cinema-seat-allocation.cpp
@@ -0,0 +1,57 @@
+class Solution {
+public:
+    int maxNumberOfFamilies(int n, vector<vector<int>>& reservedSeats) {
+
+        // Store reserved seats row-wise
+        map<int, set<int>> grid;
+        set<int> reservedRows;
+
+        for (vector<int> x : reservedSeats) {
+            int row = x[0];
+            int seat = x[1];
+
+            grid[row].insert(seat);
+            reservedRows.insert(row);
+        }
+
+        // Rows with no reservations can always accommodate 2 groups
+        int ans = (n - reservedRows.size()) * 2;
+
+        for (int row : reservedRows) {
+
+            bool left = true;   // seats 2-5
+            bool middle = true; // seats 4-7
+            bool right = true;  // seats 6-9
+
+            for (int seat = 2; seat <= 5; seat++) {
+                if (grid[row].count(seat)) {
+                    left = false;
+                    break;
+                }
+            }
+
+            for (int seat = 4; seat <= 7; seat++) {
+                if (grid[row].count(seat)) {
+                    middle = false;
+                    break;
+                }
+            }
+
+            for (int seat = 6; seat <= 9; seat++) {
+                if (grid[row].count(seat)) {
+                    right = false;
+                    break;
+                }
+            }
+
+            if (left && right) {
+                ans += 2;
+            }
+            else if (left || middle || right) {
+                ans += 1;
+            }
+        }
+
+        return ans;
+    }
+};
