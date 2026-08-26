00248bdf4c60b84be4882842a0d2f563b622c03c
Utkarsh Pratap <utkarsh.2023ug2037@iiitranchi.ac.in>
2026-08-26 19:40:55 +0530

    Time: 23 ms (84.73%), Space: 18.8 MB (19.2%) - LeetHub

---
+++ 1226-the-dining-philosophers/1226-the-dining-philosophers.cpp
@@ -0,0 +1,31 @@
+class DiningPhilosophers {
+public:
+    mutex forks[5];
+
+    void wantsToEat(
+        int philosopher,
+        function<void()> pickLeftFork,
+        function<void()> pickRightFork,
+        function<void()> eat,
+        function<void()> putLeftFork,
+        function<void()> putRightFork
+    ) {
+        int left = philosopher;
+        int right = (philosopher + 1) % 5;
+
+        // Lock both forks without deadlock
+        std::lock(forks[left], forks[right]);
+
+        // Now both forks are ours
+        pickLeftFork();
+        pickRightFork();
+
+        eat();
+
+        putLeftFork();
+        putRightFork();
+
+        forks[left].unlock();
+        forks[right].unlock();
+    }
+};
