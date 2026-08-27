c22fa3840138b5ea0935b11329066eccf2b6afb1
Utkarsh Pratap <utkarsh.2023ug2037@iiitranchi.ac.in>
2026-08-27 12:28:44 +0530

    Time: 0 ms (100%), Space: 10 MB (70.06%) - LeetHub

--- 3720-lexicographically-smallest-permutation-greater-than-target/3720-lexicographically-smallest-permutation-greater-than-target.cpp
+++ 3720-lexicographically-smallest-permutation-greater-than-target/3720-lexicographically-smallest-permutation-greater-than-target.cpp
@@ -11,14 +11,10 @@ public:
 
         string per = "";
 
-        // ----------------------------------------
-        // Try to match target from left to right
-        // ----------------------------------------
         for (int pos = 0; pos < n; pos++) {
 
             int cur = target[pos] - 'a';
 
-            // Find smallest character >= target[pos]
             char firele = '{';
 
             for (int i = cur; i < 26; i++) {
@@ -28,18 +24,15 @@ public:
                 }
             }
 
-            // Nothing >= target[pos]
             if (firele == '{') {
                 break;
             }
 
-            // If greater, directly construct answer
             if (firele > target[pos]) {
 
                 per += firele;
                 freq[firele - 'a']--;
 
-                // Remaining characters ascending
                 for (int i = 0; i < 26; i++) {
                     while (freq[i] > 0) {
                         per += char('a' + i);
@@ -50,24 +43,16 @@ public:
                 return per;
             }
 
-            // Equal -> keep matching
             per += firele;
             freq[firele - 'a']--;
         }
-
-        // ----------------------------------------
-        // We couldn't make answer greater yet.
-        // Backtrack over ONLY matched characters.
-        // ----------------------------------------
 
         int len = (int)per.length();
 
         for (int pos = len - 1; pos >= 0; pos--) {
 
-            // Give back the character at this position
             freq[per[pos] - 'a']++;
 
-            // Find smallest character > target[pos]
             char firele = '{';
 
             for (int i = target[pos] - 'a' + 1; i < 26; i++) {
@@ -81,11 +66,9 @@ public:
 
                 string ans = per.substr(0, pos);
 
-                // Put greater character here
                 ans += firele;
                 freq[firele - 'a']--;
 
-                // Put remaining characters ascending
                 for (int i = 0; i < 26; i++) {
                     while (freq[i] > 0) {
                         ans += char('a' + i);
