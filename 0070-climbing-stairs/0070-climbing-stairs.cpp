8340f4c10609f8f2a306d49fe459ba8d559da0b4
Utkarsh Pratap <utkarsh.2023ug2037@iiitranchi.ac.in>
2026-08-31 00:01:54 +0530

    Time: 0 ms (100%), Space: 8.7 MB (20.9%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int solve(int n,vector<int> &dp){
+        //base case
+        if(n<=2)
+          return n;
+        
+        if(dp[n]!=-1) 
+          return dp[n]; 
+        
+        return dp[n]=solve(n-1,dp)+solve(n-2,dp);
+    }
+    int climbStairs(int n) {
+        if(n<=2)
+         return n;
+        vector<int> dp(n+1,-1);
+        return solve(n,dp);
+    }
+};
