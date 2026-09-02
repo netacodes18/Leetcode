8217cf240fbca0d1492883044a3e81ed0b5541e2
Utkarsh Pratap <utkarsh.2023ug2037@iiitranchi.ac.in>
2026-09-02 12:07:06 +0530

    Time: 5 ms (82.6%), Space: 12.9 MB (87.79%) - LeetHub

---
+++ 1691-maximum-height-by-stacking-cuboids/1691-maximum-height-by-stacking-cuboids.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int maxHeight(vector<vector<int>>& cuboids) {
+        int n = cuboids.size();
+
+        for (auto &c : cuboids) {
+            sort(c.begin(), c.end());
+        }
+
+        sort(cuboids.begin(), cuboids.end());
+
+        vector<int> dp(n);
+
+        for (int i = 0; i < n; i++) {
+            dp[i] = cuboids[i][2];
+
+            for (int j = 0; j < i; j++) {
+
+                if (cuboids[j][0] <= cuboids[i][0] &&
+                    cuboids[j][1] <= cuboids[i][1] &&
+                    cuboids[j][2] <= cuboids[i][2]) {
+                    
+                    dp[i] = max(dp[i], dp[j] + cuboids[i][2]);
+                }
+            }
+        }
+
+        return *max_element(dp.begin(), dp.end());
+    }
+};
