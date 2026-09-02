cb79acec614dd4d9cc5bb4a035e5fe2a4537429b
Utkarsh Pratap <utkarsh.2023ug2037@iiitranchi.ac.in>
2026-09-02 19:51:43 +0530

    Time: 0 ms (100%), Space: 12.1 MB (6.61%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -6,6 +6,7 @@ public:
 
         vector<vector<int>> dp(m, vector<int>(n, 0));
 
+        // Starting cell
         if (grid[0][0] == 1)
             return 0;
 
@@ -22,15 +23,14 @@ public:
                 if (i == 0 && j == 0)
                     continue;
 
-                if (i > 0 && j > 0)
-                    dp[i][j] = dp[i-1][j] + dp[i][j-1];
-                else if (i > 0)
-                    dp[i][j] = dp[i-1][j];
-                else
-                    dp[i][j] = dp[i][j-1];
+                if (i > 0)
+                    dp[i][j] += dp[i - 1][j];
+
+                if (j > 0)
+                    dp[i][j] += dp[i][j - 1];
             }
         }
 
-        return dp[m-1][n-1];
+        return dp[m - 1][n - 1];
     }
 };
