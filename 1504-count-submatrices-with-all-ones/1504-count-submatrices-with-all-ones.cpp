4ae5f7a4edae36edd7deb14eae50347999392325
Utkarsh Pratap <utkarsh.2023ug2037@iiitranchi.ac.in>
2026-09-05 00:13:49 +0530

    Time: 27 ms (25.27%), Space: 19.2 MB (70.88%) - LeetHub

--- 1504-count-submatrices-with-all-ones/1504-count-submatrices-with-all-ones.cpp
+++ 1504-count-submatrices-with-all-ones/1504-count-submatrices-with-all-ones.cpp
@@ -1,33 +1,40 @@
 class Solution {
 public:
+
+    int countOnes(vector<int>& vec) {
+        int ans = 0;
+        int cnt = 0;
+
+        for (int x : vec) {
+            if (x == 1)
+                cnt++;
+            else
+                cnt = 0;
+
+            ans += cnt;
+        }
+
+        return ans;
+    }
+
     int numSubmat(vector<vector<int>>& mat) {
 
-        int m = mat.size();
-        int n = mat[0].size();
-
-        vector<int> nums(n, 0);
+        int n = mat.size();
+        int m = mat[0].size();
 
         int ans = 0;
 
-        for (int i = 0; i < m; i++) {
+        for (int sr = 0; sr < n; sr++) {
 
-            for (int j = 0; j < n; j++) {
+            vector<int> vec(m, 1);
 
-                // Continuous 1s ending at current cell
-                if (mat[i][j] == 0)
-                    nums[j] = 0;
-                else
-                    nums[j]++;
+            for (int er = sr; er < n; er++) {
 
-                int mn = INT_MAX;
-
-                // Expand left
-                for (int k = j; k >= 0; k--) {
-
-                    mn = min(mn, nums[k]);
-
-                    ans += mn;
+                for (int col = 0; col < m; col++) {
+                    vec[col] &= mat[er][col];
                 }
+
+                ans += countOnes(vec);
             }
         }
 
