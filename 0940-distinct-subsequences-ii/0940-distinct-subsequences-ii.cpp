2afdb5d187b9a5d67ff706aba3d874cf6134b8e1
Utkarsh Pratap <utkarsh.2023ug2037@iiitranchi.ac.in>
2026-09-07 14:38:52 +0530

    Time: 1 ms (49.08%), Space: 10.7 MB (8.94%) - LeetHub

---
+++ 0940-distinct-subsequences-ii/0940-distinct-subsequences-ii.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    const int MOD = 1e9 + 7;
+    int n;
+    int dp[2001];
+    vector<int> nextSame;
+
+    int solve(int i, string &s) {
+        if (i == n)
+            return 1;
+
+        if (dp[i] != -1)
+            return dp[i];
+
+        // Skip s[i]
+        long long skip = solve(i + 1, s);
+
+        // Take s[i]
+        long long take = solve(i + 1, s);
+
+        // If this character appeared before,
+        // remove the duplicate subsequences
+        if (nextSame[i] != -1) {
+            take -= solve(nextSame[i] + 1, s);
+        }
+
+        return dp[i] = (skip + take) % MOD;
+    }
+
+    int distinctSubseqII(string s) {
+        n = s.length();
+
+        memset(dp, -1, sizeof(dp));
+
+        nextSame.assign(n, -1);
+
+        vector<int> last(26, -1);
+
+        // Find next occurrence of every character
+        for (int i = n - 1; i >= 0; i--) {
+            int ch = s[i] - 'a';
+
+            nextSame[i] = last[ch];
+
+            last[ch] = i;
+        }
+
+        return (solve(0, s) - 1 + MOD) % MOD;
+    }
+};
